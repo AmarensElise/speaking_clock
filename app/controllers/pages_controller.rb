8c681c8ac0a4c834b6e38480e34101e1785655cd
Amarens <52682778+AmarensElise@users.noreply.github.com>
2021-10-25 16:21:18 +0200

    changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -64,4 +64,112 @@ class PagesController < ApplicationController
 
     @filepath = "/assets/yoda/" + @hour_twelve.to_s + "_" + @minutes.to_s + ".mp3"
   end
+
+  def russian
+    @current_hour =  Time.now.hour
+
+    if @current_hour > 12
+      @hour_twelve = @current_hour - 12
+    else
+      @hour_twelve = @current_hour
+    end
+
+    if Time.now.min.between?(0, 2)
+      @minutes = 0
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(3, 7)
+      @minutes = 5
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(8, 12)
+      @minutes = 10
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(13, 17)
+      @minutes = 15
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(18, 22)
+      @minutes = 20
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(23, 27)
+      @minutes = 25
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(28, 32)
+      @minutes = 30
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(33, 37)
+      @minutes = 25
+      @hour = @hour_twelve + 1
+    elsif Time.now.min.between?(38, 42)
+      @minutes = 20
+      @hour = @hour_twelve + 1
+    elsif Time.now.min.between?(43, 47)
+      @minutes = 15
+      @hour = @hour_twelve + 1
+    elsif Time.now.min.between?(48, 52)
+      @minutes = 10
+      @hour = @hour_twelve + 1
+    elsif Time.now.min.between?(53, 57)
+      @minutes = 5
+      @hour = @hour_twelve + 1
+    elsif Time.now.min.between?(58, 59)
+      @minutes = 5
+      @hour = @hour_twelve + 1
+    end
+
+    @time_phrase = "It's " + @hour.to_s + ":" + @minutes.to_s
+    @filepath = "/assets/russian/" + @hour_twelve.to_s + "_" + @minutes.to_s + ".mp3"
+  end
+
+  def french
+    @current_hour =  Time.now.hour
+
+    if @current_hour > 12
+      @hour_twelve = @current_hour - 12
+    else
+      @hour_twelve = @current_hour
+    end
+
+    if Time.now.min.between?(0, 2)
+      @minutes = 0
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(3, 7)
+      @minutes = 5
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(8, 12)
+      @minutes = 10
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(13, 17)
+      @minutes = 15
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(18, 22)
+      @minutes = 20
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(23, 27)
+      @minutes = 25
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(28, 32)
+      @minutes = 30
+      @hour = @hour_twelve
+    elsif Time.now.min.between?(33, 37)
+      @minutes = 25
+      @hour = @hour_twelve + 1
+    elsif Time.now.min.between?(38, 42)
+      @minutes = 20
+      @hour = @hour_twelve + 1
+    elsif Time.now.min.between?(43, 47)
+      @minutes = 15
+      @hour = @hour_twelve + 1
+    elsif Time.now.min.between?(48, 52)
+      @minutes = 10
+      @hour = @hour_twelve + 1
+    elsif Time.now.min.between?(53, 57)
+      @minutes = 5
+      @hour = @hour_twelve + 1
+    elsif Time.now.min.between?(58, 59)
+      @minutes = 5
+      @hour = @hour_twelve + 1
+    end
+
+    @time_phrase = "It's " + @hour.to_s + ":" + @minutes.to_s
+    @filepath = "/assets/french/" + @hour_twelve.to_s + "_" + @minutes.to_s + ".mp3"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root :to => "pages#home"
 
   get 'pages/home'
+  get 'pages/french'
+  get 'pages/russian'
   get 'pages/yoda'
+
+  get 'pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
